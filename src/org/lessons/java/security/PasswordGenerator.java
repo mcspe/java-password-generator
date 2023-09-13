f6ba5749768c8867efe5b4162bb3c919c4b2d414
mcspe <mcspezzaferro@gmail.com>
2023-09-13 16:07:56 +0200

    [add] project

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,10 +1,34 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("BENVENUTO SU PASSWORD GENERATOR");
+		
+		System.out.print("Inserisci il tuo nome: ");
+		String userName = sc.nextLine().trim().toLowerCase();
+		
+		System.out.print("Inserisci il tuo cognome: ");
+		String userLastname = sc.nextLine().trim().toLowerCase();
+		
+		System.out.print("Qual è il tuo colore preferito? ");
+		String favColor = sc.nextLine().trim().toLowerCase();
+		
+		System.out.print("Qual è la tua data di nascita? ");
+		String doB = sc.nextLine();
+		String[] splitDoB = doB.split("[-/]");
+		int day = Integer.parseInt(splitDoB[0]);
+		int month = Integer.parseInt(splitDoB[1]);
+		int year = Integer.parseInt(splitDoB[2]);
+		int sumDoB = day + month + year;
+		
+		String password = userName + '-' + userLastname + '-' + favColor + '-' + sumDoB;
+		System.out.println("La tua password è: " + password);
 	}
 
 }
